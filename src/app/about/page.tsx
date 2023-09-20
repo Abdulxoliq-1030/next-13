f603ddf377242b2f29049ebca7f469690935d162
Abdulxoliq Odiljonov <abdulxoliqodiljonov@Abdulxoliqs-MacBook-Pro.local>
2023-09-20 20:36:23 +0500

    next js 13 init code

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+interface AboutProps { }
+
+const About: React.FC<AboutProps> = () => <h1>About Component</h1>
+
+export default About;
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+interface ContactProps { }
+
+const Contact: React.FC<ContactProps> = () => <h1>Contact Component</h1>
+
+export default Contact;
